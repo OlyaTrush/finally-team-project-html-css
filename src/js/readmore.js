c344b678239cbcf13ee5c504aacc73267a003e20
mstryuts <maxim.stryuts@gmail.com>
2022-05-31 22:42:56 +0300

    js up

--- src/js/readmore.js
+++ src/js/readmore.js
@@ -1,5 +1,5 @@
-function btn_onclick(btn) {
-    let more = document.querySelector(".more");
+window.btn_onclick = function btn_onclick(btn, selector) {
+    let more = document.querySelector(selector);
     const moreDisplayStyle = "block";
     if (more.style.display === moreDisplayStyle) {
         btn.innerHTML = "Read more";
