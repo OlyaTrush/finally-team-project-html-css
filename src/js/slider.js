2e4c1a5d7c6690b013835707b991f2ace2e78c71
Andrey Kondratenko <delo.ua2@gmail.com>
2022-06-01 09:36:06 +0300

    slider created

---
+++ src/js/slider.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 32,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+    clickable: true,
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+});
